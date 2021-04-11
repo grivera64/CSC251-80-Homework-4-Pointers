ebfa18c0db9ab746cc6150c013209e168c411554
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-11 01:35:15 -0700

    Merge feat/find-closest-flight to main
    Squashed commit of the following:
    
    commit 5fd2385d093da24488a007bf173529a99d5c61cc
    Author: Giovanni Rivera <grivera64@toromail.csudh.edu>
    Date:   Sun Apr 11 01:31:46 2021 -0700
    
        Refactor(flight): Remove unnecessary continue
    
    commit 45234890d1bfbc731dda5245343c2375cb0c8cb7
    Author: Giovanni Rivera <grivera64@toromail.csudh.edu>
    Date:   Sun Apr 11 01:28:19 2021 -0700
    
        Fix(flight): Edit find_cl... function
    
        Fixes error from pass-by-reference vs pass-by-value
    
    commit 1ba429aaa8f6195235de91f67c13e444aac1613e
    Author: Giovanni Rivera <grivera64@toromail.csudh.edu>
    Date:   Sun Apr 11 00:07:25 2021 -0700
    
        Feat(flight)!: Add implementation to find_clos...
    
        The implementation breaks the program at the last few lines when
        transferring the pointer values to each other
    
        BREAKS pointers attached
    
    commit 682e5f610fbe98f4a084c30b17bc68b8db68765a
    Author: Giovanni Rivera <grivera64@toromail.csudh.edu>
    Date:   Sat Apr 10 23:24:02 2021 -0700
    
        Feat(flight): Store depart/arrive times as minutes
    
    commit fe4e2f7916c5258aa930c583892f5c78afbbbe60
    Author: Giovanni Rivera <grivera64@toromail.csudh.edu>
    Date:   Sat Apr 10 23:10:29 2021 -0700
    
        Feat(flight): Add prototype of find-closest-flight

--- flight/closestFlight.c
+++ flight/closestFlight.c
@@ -9,15 +9,40 @@
 
 /* Macros */
 #include <stdio.h>
+#include <math.h>
+#define SIZE 8
 
 /* Function prototypes */
 void printChart();                                              //prints daily flights table to terminal
 void to_minutes(int hours, int minutes, int *total);            //converts hh:mm to minutes
 
+void find_closest_flight(int desired_time, int *departure_time, int *arrival_time);
+
 int main()
 {
 
     int hours, minutes, total;
+
+    int departures[SIZE] = {0};
+    int arrivals[SIZE] = {0};
+
+    to_minutes(8, 0, &departures[0]);
+    to_minutes(9, 43, &departures[1]);
+    to_minutes(11, 19, &departures[2]);
+    to_minutes(0, 47, &departures[3]);
+    to_minutes(14, 0, &departures[4]);
+    to_minutes(15, 45, &departures[5]);
+    to_minutes(19, 0, &departures[6]);
+    to_minutes(21, 45, &departures[7]);
+
+    to_minutes(10, 16, &arrivals[0]);
+    to_minutes(11, 52, &arrivals[1]);
+    to_minutes(1, 31, &arrivals[2]);
+    to_minutes(15, 0, &arrivals[3]);
+    to_minutes(16, 8, &arrivals[4]);
+    to_minutes(17, 55, &arrivals[5]);
+    to_minutes(9, 20, &arrivals[6]);
+    to_minutes(23, 58, &arrivals[7]);
 
     printChart();
 
@@ -27,6 +52,17 @@ int main()
     to_minutes(hours, minutes, &total);
 
     printf("%d\n", total);
+
+    int *depart, *arrive;
+
+    depart = &departures[0];
+    arrive = &arrivals[0];
+
+    printf("Original depart is %d\n", *depart);
+
+    find_closest_flight(total, depart, arrive);
+
+    printf("The closest time to %d is %d\n", total, *depart);
 
 
     return 0;
@@ -53,5 +89,31 @@ void to_minutes(int hours, int minutes, int *total)
 
     *total = hours * 60;
     *total += minutes;
+
+}
+
+void find_closest_flight(int desired_time, int *departure_time, int *arrival_time)
+{
+
+    int *depart_pair = departure_time;
+    int *arrival_pair = arrival_time;
+
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+
+        if(abs(*(depart_pair + i) - desired_time) > abs(*(depart_pair + i + 1) - desired_time))
+        {
+
+            depart_pair = departure_time + i + 1;
+            arrival_pair = arrival_time + i + 1;
+            
+        }
+
+    }
+
+    *departure_time = *depart_pair;
+    *arrival_time = *arrival_pair;
+
+    printf("Should point to %d and %d\n", *departure_time, *arrival_time);
 
 }
