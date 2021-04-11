5d4aa35690a78e181edbb67504af1f18c7730f98
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-11 02:03:00 -0700

    Fix(flight): Fix find_cl... in closestFlight.c
    Changes the incorrect logic in the if statment of find_closest_flight

--- flight/closestFlight.c
+++ flight/closestFlight.c
@@ -126,7 +126,7 @@ void find_closest_flight(int desired_time, int *departure_time, int *arrival_tim
             Check if the difference for the current depart time 
             is greater than the new depart time
         */
-        if(abs(*(depart_pair + i) - desired_time) > abs(*(depart_pair + i + 1) - desired_time))
+        if(abs(*(departure_time + i) - desired_time) > abs(*(departure_time + i + 1) - desired_time))
         {
 
             /* If the current is greater, set the depart_pair to the next one */
