b1483de5b0ba04d7b56667b07ae821705d341def
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-11 22:59:51 -0700

    Fix(flight): Fix output of 12am and 12pm

--- flight/closestFlight.c
+++ flight/closestFlight.c
@@ -78,8 +78,8 @@ int main()
     to_clock(*arrive, &arrive_hours, &arrive_minutes);
 
     printf("The closest departure time is %d:%02d %s, arriving at %d:%02d %s\n", 
-            depart_hours % 12, depart_minutes, (depart_hours > 12) ? "p.m." : "a.m.", 
-            arrive_hours % 12, arrive_minutes, (depart_hours > 12) ? "p.m." : "a.m.");
+            (depart_hours % 12) ? depart_hours % 12 : 12, depart_minutes, (depart_hours > 12) ? "p.m." : "a.m.", 
+            (arrive_hours % 12) ? arrive_hours % 12 : 12, arrive_minutes, (depart_hours > 12) ? "p.m." : "a.m.");
 
     /* Terminate the program */
     return 0;
