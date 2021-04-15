84cbe483b438a452cabf8b85021cdf86ca27a774
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-15 01:42:09 -0700

    Refactor(quick): Change hardcoded scanf to loop
    Allows for easy change of array sizes

--- quick/qsort.c
+++ quick/qsort.c
@@ -25,8 +25,13 @@ int main(void)
 
     //requesting for user input
     printf("Enter %d values to sort: ", N);
-    scanf("%d %d %d %d %d %d %d", 
-    &arr[0], &arr[1], &arr[2], &arr[3], &arr[4], &arr[5], &arr[6]);
+
+    for (int i = 0; i < N; i++)
+    {
+        
+        scanf("%d", &arr[i]);
+    
+    }
 
     //calling quick sort alorithm and passing array by reference
     quick_sort(&arr[0], &arr[N - 1], &arr[0], &arr[N - 1]);
